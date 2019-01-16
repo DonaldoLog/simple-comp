f8f3256d59af26a809cbc930bb9c6b6c71aa192d
Donaldo Lopez <donaldolog@gmail.com>
2019-01-15 23:23:04 -0600

    first commit

---
+++ src/components/ComponenteHijo.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    div
+        h1 Este es un componente hijo
+        componente-local
+</template>
+
+<script>
+  import ComponenteLocal from './ComponenteLocal.vue'
+export default {
+    components: {
+        ComponenteLocal
+    }
+    
+}
+</script>
